c8f253fa493bcddb260402d5d3298d7252f2b68f
ediblecode <ian.routledge@nice.org.uk>
2017-12-21 08:12:13 +0000

    Add new page structure

--- server/index.js
+++ server/index.js
@@ -6,11 +6,15 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 app.set("port", PORT);
 
-console.log("Express");
+// Avoid 404 error in console
+app.get("/setgtmdatalayer", function (req, res) {
+	res.setHeader("Content-Type", "application/javascript");
+	res.send("var dataLayer = [{\"uid\":null,\"persona\":null}]");
+});
 
 app.use("/fonts", express.static(path.join(__dirname, "./../node_modules/@nice-digital/icons/dist")));
 app.use("/css", express.static(path.join(__dirname, "../temp")));
-app.use(express.static(path.join(__dirname, "./../")));
+app.use(express.static(path.join(__dirname, "./../pages")));
 
 app.listen(app.get("port"), function(){
 	console.log("Express server listening on port " + app.get("port"));
