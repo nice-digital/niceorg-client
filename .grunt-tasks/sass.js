4372af167bce0f9f619f130ba4125482ab2c24ac
ediblecode <ian.routledge@nice.org.uk>
2018-05-16 16:07:50 +0100

    GW-92 Reorganise watch task with shared options

--- .grunt-tasks/sass.js
+++ .grunt-tasks/sass.js
@@ -1,30 +1,20 @@
 module.exports = {
+	options: {
+		sourceMap: true,
+		includePaths: [
+			"src",
+			"node_modules/@nice-digital/design-system/src/stylesheets",
+			"node_modules/@nice-digital/icons/dist"
+		],
+		outputStyle: "nested",
+		sourceMapContents: true
+	},
 	temp: {
-		options: {
-			sourceMap: true,
-			includePaths: [
-				"src",
-				"node_modules/@nice-digital/design-system/src/stylesheets",
-				"node_modules/@nice-digital/icons/dist"
-			],
-			outputStyle: "nested",
-			sourceMapContents: true
-		},
 		files: {
 			"temp/app.css": "src/app.scss"
 		}
 	},
 	dist: {
-		options: {
-			sourceMap: true,
-			includePaths: [
-				"src",
-				"node_modules/@nice-digital/design-system/src/stylesheets",
-				"node_modules/@nice-digital/icons/dist"
-			],
-			outputStyle: "nested",
-			sourceMapContents: true
-		},
 		files: {
 			"dist/app.css": "src/app.scss"
 		}
