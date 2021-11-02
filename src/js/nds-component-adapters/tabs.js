84756415c16b2f180bbd0015d4128e06e8978672
w@rren <w@rren.dev>
2021-11-02 16:39:53 +0000

    NOC-46 Remove setting URL hash from tabs click

--- src/js/nds-component-adapters/tabs.js
+++ src/js/nds-component-adapters/tabs.js
@@ -4,14 +4,8 @@ import ReactDOM from "react-dom";
 import { Tabs, Tab } from "@nice-digital/nds-tabs";
 import { getPropsFromDataAttributes } from "../utils";
 
-function setHashToCurrentTab(hash) {
-	document.location.hash = hash;
-}
-
 const render = (element) => {
 	const props = getPropsFromDataAttributes(element, "tabs");
-	props.hash = document.location.hash || "";
-	props.changeCallback = setHashToCurrentTab;
 	const tabs = Array.prototype.map.call(element.children, (tab) => ({
 		title: tab.getAttribute("data-tab-title"),
 		body: tab.innerHTML
